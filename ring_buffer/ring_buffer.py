143aa275bd40808002340b8e8a096be74c847d88
droneal11015 <droneal11015@gmail.com>
2019-09-20 14:39:54 -0400

    fixed error in ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -12,7 +12,8 @@ class RingBuffer:
     #if ring buffer is full (available capacity is 0), this adds an element to the array and sets current to the first index
       elif self.current == self.capacity - 1:
           self.current = 0
+      print(self.storage)
 
   def get(self):
     #returns item if index is not None
-      print([item for item in self.storage if item is not None])
+      return ([item for item in self.storage if item is not None])
